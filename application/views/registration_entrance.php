59be1fba716183f09e40de0a17391da610a39ae4
JeldorPKU <jack.jiang.capu@gmail.com>
2015-03-26 22:53:19 +0800

    Remove useless style container.

--- application/views/registration_entrance.php
+++ application/views/registration_entrance.php
@@ -1,4 +1,4 @@
-<div class="container btn-group-lg btn-group">
+<div class="btn-group-lg btn-group">
   <button type="button" class="btn btn-primary">资料</button>
   <button type="button" class="btn btn-primary">人员信息填报</button>
   <button type="button" class="btn btn-primary">团体赛信息填报</button>
