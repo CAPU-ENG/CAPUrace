969cb17d84edbaa1238137e45634f871fc0ef85d
JeldorPKU <jack.jiang.capu@gmail.com>
2015-03-26 10:39:40 +0800

    Enable adding and deleting to team form.

--- application/views/registration_team.php
+++ application/views/registration_team.php
@@ -1,31 +1,44 @@
 <div class="regcontainer">
     <h3>请录入团队信息</h3>
     <hr/>
-    <label class="col-sm-1">序号</label>
-    <label class="col-sm-3">第一棒</label>
-    <label class="col-sm-3">第二棒</label>
-    <label class="col-sm-3">第三棒</label>
-    <br/><br/>
-    <div class="team-form hidden">
-        <div class="col-sm-1"></div>
-        <div class="col-sm-3">
-            <select class="form-control" name="first">
+    <div class="form-group reg">
+        <label class="col-sm-1">序号</label>
+        <label class="col-sm-3">第一棒</label>
+        <label class="col-sm-3">第二棒</label>
+        <label class="col-sm-3">第三棒</label>
+        <br/><br/>
+        <div class="team-form hidden">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-3">
+                <select class="form-control" name="first">
 
-            </select>
-        </div>
-        <div class="col-sm-3">
-            <select class="form-control" name="second">
+                </select>
+            </div>
+            <div class="col-sm-3">
+                <select class="form-control" name="second">
 
-            </select>
-        </div>
-        <div class="col-sm-3">
-            <select class="form-control" name="third">
+                </select>
+            </div>
+            <div class="col-sm-3">
+                <select class="form-control" name="third">
 
-            </select>
-        </div>
-        <div class="col-sm-2">
-            <button class="btn btn-danger btn-block">删除</button>
+                </select>
+            </div>
+            <div class="col-sm-2">
+                <button class="btn btn-danger btn-block" onclick="removeTeam($(this))">删除</button>
+            </div>
+            <br/>
+            <br/>
         </div>
     </div>
-
+    <hr/>
+    <div class="col-sm-4">
+        <button class="btn btn-primary btn-block" onclick="addTeam()">添加一个团队</button>
+    </div>
+    <div class="col-sm-4">
+        <button class="btn btn-warning btn-block">暂时保存</button>
+    </div>
+    <div class="col-sm-4">
+        <button class="btn btn-success btn-block">提交</button>
+    </div>
 </div>
--- assets/js/race.js
+++ assets/js/race.js
@@ -12,5 +12,18 @@ function removeIndividual(item) {
     item.closest(".individual-form").remove();
 }
 
+/*
+ * This function add a new team form.
+ */
+function addTeam() {
+    $(".reg").append($(".team-form:first").clone(true).removeClass("hidden"));
+}
+
+/*
+ * This function removes an existing team item.
+ */
+function removeTeam(item) {
+    item.closest(".team-form").remove();
+}
 /* End of file race.js */
 /* Location: ./assets/js/race.js */
