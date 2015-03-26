793fb6f6b9f9de32b5b04341607abf0ff7a7ced2
JeldorPKU <jack.jiang.capu@gmail.com>
2015-03-26 11:44:32 +0800

    Add registration entrance.

--- application/controllers/registration.php
+++ application/controllers/registration.php
@@ -10,7 +10,7 @@ class Registration extends CI_Controller {
 
     public function index() {
         $this->load->view('header');
-        $this->load->view('registration_individual');
+        $this->load->view('registration_entrance');
         $this->load->view('footer');
     }
 
---
+++ application/views/registration_entrance.php
@@ -0,0 +1,5 @@
+<div class="container btn-group-lg btn-group">
+  <button type="button" class="btn btn-primary">注册</button>
+  <button type="button" class="btn btn-primary">登录</button>
+  <button type="button" class="btn btn-primary">管理员</button>
+</div>
