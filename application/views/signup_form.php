2002b322b29a3d08135b02ad5bd713a653bfc4fa
JeldorPKU <jack.jiang.capu@gmail.com>
2015-03-26 09:08:19 +0800

    Remove useless comments.

--- application/views/signup_form.php
+++ application/views/signup_form.php
@@ -1,5 +1,3 @@
-<?php echo validation_errors(); ?>
-
 <div class="signcontainer">
     <h3>请输入注册信息</h3>
     <hr/>
@@ -44,24 +42,4 @@
     <div class="col-sm-6">
         <button class="btn btn-success btn-block">注册</button>
     </div>
-
-
-
 </div>
-
-<!--
---><?php
-/*
-echo form_open(site_url('user/signup'));
-echo '学校' . form_input('school', set_value('school')) . br();
-echo '车协名称' . form_input('association_name', set_value('association_name')) . br();
-echo '所在地区（省级行政区）' . form_dropdown('province', $GLOBALS['PROVINCES'], set_value('province')) . br();
-echo '领队姓名' . form_input('leader', set_value('leader')) . br();
-echo '联系电话' . form_input('tel', set_value('tel')) . br();
-echo '电子邮箱（登录名）' . form_input('mail', set_value('mail')) . br();
-echo '密码' . form_password('password', '') . br();
-echo '再次输入密码' . form_password('passconf', '') . br();
-echo form_submit('', '注册') . form_reset('', '重置');
-echo form_close();
-
-*/?>
