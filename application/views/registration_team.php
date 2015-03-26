1a74a6879771fc19d8f9cb7e14cceb5b29a73333
JeldorPKU <jack.jiang.capu@gmail.com>
2015-03-26 08:58:35 +0800

    Basic team form.

---
+++ application/views/registration_team.php
@@ -0,0 +1,31 @@
+<div class="regcontainer">
+    <h3>请录入团队信息</h3>
+    <hr/>
+    <label class="col-sm-1">序号</label>
+    <label class="col-sm-3">第一棒</label>
+    <label class="col-sm-3">第二棒</label>
+    <label class="col-sm-3">第三棒</label>
+    <br/><br/>
+    <div class="team-form hidden">
+        <div class="col-sm-1"></div>
+        <div class="col-sm-3">
+            <select class="form-control" name="first">
+
+            </select>
+        </div>
+        <div class="col-sm-3">
+            <select class="form-control" name="second">
+
+            </select>
+        </div>
+        <div class="col-sm-3">
+            <select class="form-control" name="third">
+
+            </select>
+        </div>
+        <div class="col-sm-2">
+            <button class="btn btn-danger btn-block">删除</button>
+        </div>
+    </div>
+
+</div>
