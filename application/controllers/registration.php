53fea23fcbba299fd2e570049c5e9df066d62832
huxuan <i@huxuan.org>
2015-04-01 20:18:04 +0800

    View of reg index should work now.

--- application/controllers/registration.php
+++ application/controllers/registration.php
@@ -24,7 +24,7 @@ class Registration extends CI_Controller {
 
     public function index() {
         $this->load->view('header');
-        $this->load->view('registration_entrance');
+        $this->load->view('registration_index');
         $this->load->view('footer');
     }
 
---
+++ application/views/registration_index.php
@@ -0,0 +1,28 @@
+<div class="regcontainer">
+  <h3>报名须知</h3>
+  <p class="text-justify">
+    balabala
+    <br/>
+    balabala
+  </p>
+  <div class="row">
+    <div class="col-sm-4">
+    </div>
+    <div class="col-sm-4 text-center">
+      <div class="checkbox">
+        <label><input type="checkbox" value="">我已阅读并同意以上协议</label>
+      </div>
+    </div>
+    <div class="col-sm-4">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+    </div>
+    <div class="col-sm-4">
+      <button type="submit" class="btn btn-success btn-block">开始报名</button>
+    </div>
+    <div class="col-sm-4">
+    </div>
+  </div>
+</div>
