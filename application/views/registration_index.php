6ff48f274097c4c4d9787704389022b8e18818b5
huxuan <i@huxuan.org>
2015-04-02 22:39:38 +0800

    Checkbox and button disabled should work in registration index page.

--- application/views/registration_index.php
+++ application/views/registration_index.php
@@ -10,7 +10,7 @@
     </div>
     <div class="col-sm-4 text-center">
       <div class="checkbox">
-        <label><input type="checkbox" value="">我已阅读并同意以上协议</label>
+        <label><input type="checkbox" id="checkbox-reg-agree">我已阅读并同意以上协议</label>
       </div>
     </div>
     <div class="col-sm-4">
@@ -20,9 +20,14 @@
     <div class="col-sm-4">
     </div>
     <div class="col-sm-4">
-      <button type="submit" class="btn btn-success btn-block">开始报名</button>
+      <button type="submit" class="btn btn-success btn-block" id="btn-reg-agree" disabled>开始报名</button>
     </div>
     <div class="col-sm-4">
     </div>
   </div>
 </div>
+<script>
+    $("#checkbox-reg-agree").change(function() {
+        $('#btn-reg-agree').prop('disabled', !this.checked);
+    });
+</script>
