04930126eeebbc689e88ef4861ffa4d8e87de0b6
huxuan <i@huxuan.org>
2015-04-08 01:40:13 +0800

    Use 126 mail as default reply_to mail address.

--- application/libraries/MY_Email.php
+++ application/libraries/MY_Email.php
@@ -38,6 +38,7 @@ class MY_Email extends CI_Email
         $from_name = $options['from_name'];
 
         $this->from($from_mail, $from_name);
+        $this->reply_to('beidachexie@126.com', $from_name);
         $this->to($to_mail);
         $this->subject($subject);
         $this->message($message);
