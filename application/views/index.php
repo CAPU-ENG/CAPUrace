408956e3155698c03f2f67d3e8e5ddf1d40628f4
范志康 <goodman.capu@gmail.com>
2015-04-09 22:12:48 +0800

    首页

--- application/views/index.php
+++ application/views/index.php
@@ -1,1 +1,27 @@
-<h1>欢迎参加北大车协全国高校山地车交流赛</h1>
+<body style="background:url('http://ww2.sinaimg.cn/large/0063uqpDjw1eqnm3enisyj31kw11xarl.jpg')top center;"><center>
+
+<br><br><br>
+<div style="filter:alpha(opacity=80);-moz-opacity:0.8;opacity:0.8;background:#F5F5F5;padding:10px;">
+    <h1>欢迎参加北大车协全国高校山地车交流赛</h1>
+    <h3>比赛时间：2015年5月16日-17日</h3>
+    <h3>比赛地点：固安永定河自行车运动公园</h3>
+</div>
+
+<div class="right">
+    <br><br><br>
+    <div>
+        <a href="http://ww1.sinaimg.cn/large/0063uqpDjw1eqkhtsd9hzj31kw16oamp.jpg" target="_blank"><img src="http://ww1.sinaimg.cn/large/0063uqpDjw1eqkhtsd9hzj31kw16oamp.jpg" width="480"></a>
+    </div>
+
+    <br><br><br>
+    <div>
+        <a href="http://ww1.sinaimg.cn/large/0063uqpDjw1eqkhtsd9hzj31kw16oamp.jpg" target="_blank"><img src="http://ww1.sinaimg.cn/large/0063uqpDjw1eqkhtsd9hzj31kw16oamp.jpg" width="480"></a>
+    </div>
+
+    <br><br><br>
+    <div>
+        <embed src="http://player.youku.com/player.php/sid/XODg4NTEyNjQw/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+    </div>
+</div>
+
+</center></body>
