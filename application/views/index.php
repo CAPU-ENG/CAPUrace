f33f5817ba4b0ce97da0d9dbadbeb23537023574
范志康 <goodman.capu@gmail.com>
2015-04-10 10:08:12 +0800

    Use CSS to set background

--- application/views/index.php
+++ application/views/index.php
@@ -1,4 +1,12 @@
-<body style="background:url('http://ww2.sinaimg.cn/large/0063uqpDjw1eqnm3enisyj31kw11xarl.jpg')top center;"><center>
+<style type="text/css">
+body {
+    text-align:center;
+    background-image:url("http://ww2.sinaimg.cn/large/0063uqpDjw1eqnm3enisyj31kw11xarl.jpg");
+    background-repeat:no-repeat;
+    background-attachment:fixed;
+    background-position:center;
+}
+</style>
 
 <br><br><br>
 <div style="filter:alpha(opacity=80);-moz-opacity:0.8;opacity:0.8;background:#F5F5F5;padding:10px;">
@@ -7,7 +15,7 @@
     <h3>比赛地点：固安永定河自行车运动公园</h3>
 </div>
 
-<div class="right">
+<div>
     <br><br><br>
     <div>
         <a href="http://ww1.sinaimg.cn/large/0063uqpDjw1eqkhtsd9hzj31kw16oamp.jpg" target="_blank"><img src="http://ww1.sinaimg.cn/large/0063uqpDjw1eqkhtsd9hzj31kw16oamp.jpg" width="480"></a>
@@ -23,5 +31,3 @@
         <embed src="http://player.youku.com/player.php/sid/XODg4NTEyNjQw/v.swf" allowFullScreen="true" quality="high" width="480" height="400" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
     </div>
 </div>
-
-</center></body>
