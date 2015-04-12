c2c19e544b10c26ef199227b75e4f0a830436a20
JeldorPKU <jack.jiang.capu@gmail.com>
2015-04-12 13:05:15 +0800

    Add individual key to the individual data. Fix #29.

--- application/models/people_model.php
+++ application/models/people_model.php
@@ -30,7 +30,8 @@ class People_model extends CI_Model {
      * Add a new person.
      */
     public function add_people($data, $school_id) {
-        $data = array_merge($data, array('school_id' => $school_id));
+        $this->load->helper(array('lib'));
+        $data = array_merge($data, array('school_id' => $school_id, 'key' => individual_encode($data)));
         $this->db->insert('people', $data);
     }
 
