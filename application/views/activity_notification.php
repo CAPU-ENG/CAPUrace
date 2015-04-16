679b93dfdbd2edce4613841b0767ef978b2e966c
cindywang319 <wxyeecs@gmail.com>
2015-04-16 23:09:02 +0800

    Changed pdf to images

--- application/views/activity_notification.php
+++ application/views/activity_notification.php
@@ -1,4 +1,7 @@
 <div class="regcontainer">
-  <iframe src="<?=base_url()?>/assets/images/activity_notification.pdf" class="center-block" width="800px" height="3350px" style="margin-top:0px"></iframe>
-  
+	<img src="/assets/images/activity_notification_1.jpg" width="800" class="center-block">
+	<hr>
+	<img src="/assets/images/activity_notification_2.jpg" width="800" class="center-block">
+	<hr>
+	<img src="/assets/images/activity_notification_3.jpg" width="800" class="center-block">
 </div>
