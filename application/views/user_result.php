b2506ebf7b398e20fa48478377d6f3c4f83fee1c
JeldorPKU <jack.jiang.capu@gmail.com>
2015-04-24 22:22:01 +0800

    Add exporting button in the result page.

--- application/views/user_result.php
+++ application/views/user_result.php
@@ -120,15 +120,17 @@
     <div style="margin:0 auto;">
         <div class="col-sm-3"></div>
         <div class="col-sm-2">
-            <?php if ($editable): ?>
+            <?php if ($editable) { ?>
             <button id="btn-go-to-pay" class="btn btn-block btn-success">前往支付</button>
-            <?php else: ?>
+            <?php } else { ?>
             <button id="btn-check-payment" class="btn btn-block btn-success">查看支付信息</button>
-            <?php endif; ?>
+            <?php }; ?>
         </div>
         <div class="col-sm-2">
             <?php if ($editable): ?>
             <button id="btn-return-to-signup" class="btn btn-block btn-warning">修改报名信息</button>
+            <?php else: ?>
+            <button id="btn-export-excel" class="btn btn-block btn-warning">导出Excel表格</button>
             <?php endif; ?>
         </div>
         <div class="col-sm-2">
