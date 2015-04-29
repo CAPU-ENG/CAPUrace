17b15e71cb35ce52a2c2d821be62fabbd245fec7
JeldorPKU <jack.jiang.capu@gmail.com>
2015-04-29 19:52:44 +0800

    Show global data.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -54,4 +54,25 @@ class Admin extends CI_Controller {
         $this->session->unset_userdata('admin_in');
         redirect(site_url('user/admin'));
     }
+
+    public function info() {
+        $data['nschools'] = $this->db->query('select count(*) as nschools from users;')->result_array()[0]['nschools'];
+        $data['nverified'] = $this->db->query('select count(*) as nverified from users where editable=0;')->result_array()[0]['nverified'];
+        $data['npaid'] = $this->db->query('select count(*) as npaid from users where paid=1;')->result_array()[0]['npaid'];
+        $data['nlook'] = $this->db->query('select count(*) as nlook from people where deleted=0 and ifrace=0;')->result_array()[0]['nlook'];
+        $data['nrace'] = $this->db->query('select count(*) as nrace from people where deleted=0 and ifrace=1;')->result_array()[0]['nrace'];
+        $data['nmale'] = $this->db->query('select count(*) as nmale from people where deleted=0 and ifrace=1 and race=1;')->result_array()[0]['nmale'];
+        $data['nfemale'] = $this->db->query('select count(*) as nfemale from people where deleted=0 and ifrace=1 and race=2;')->result_array()[0]['nfemale'];
+        $data['nteams'] = $this->db->query('select count(*) as nteams from team where deleted=0;')->result_array()[0]['nteams'];
+        $accommodation = $this->db->query('select count(*) as live from people where deleted=0 group by accommodation;')->result_array();
+        $data['hotel'] = $accommodation[1]['live'];
+        $data['tent_rent'] = $accommodation[2]['live'];
+        $data['tent_bring'] = $accommodation[3]['live'];
+        $data['meal16'] = $this->db->query('select count(*) as meal16 from people where deleted=0 and meal16=1;')->result_array()[0]['meal16'];
+        $data['meal17'] = $this->db->query('select count(*) as meal17 from people where deleted=0 and meal17=1;')->result_array()[0]['meal17'];
+
+        $this->load->view('header_admin');
+        $this->load->view('admin_info', $data);
+        $this->load->view('footer_admin');
+    }
 }
