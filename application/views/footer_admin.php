58c0ee47b42289485ff1d79de03922472e6c9460
JeldorPKU <jack.jiang.capu@gmail.com>
2015-04-29 20:05:49 +0800

    Add global data navigation.

--- application/views/footer_admin.php
+++ application/views/footer_admin.php
@@ -1,21 +1,14 @@
 <div class="container">
     <div class="col-sm-1"></div>
-    <div class="col-sm-2">
-        <button class="btn btn-primary btn-block" id="btn-return-to-admin-index">管理员首页</button>
-    </div>
-    <div class="col-sm-2">
-        <button class="btn btn-primary btn-block" id="btn-go-to-pay">缴费后台</button>
-    </div>
-    <div class="col-sm-2">
-        <button class="btn btn-primary btn-block disabled" id="btn-export-excel">导出总表</button>
-    </div>
-    <div class="col-sm-2">
-        <button class="btn btn-warning btn-block" id="btn-logout">注销</button>
-    </div>
-    <div class="col-sm-2">
-        <button class="btn btn-danger btn-block disabled" id="btn-close-system">关闭系统</button>
-    </div>
-    <div class="col-sm-1"></div>
+        <button class="btn btn-primary" id="btn-return-to-admin-index">管理员首页</button>
+        <div class="btn-group">
+            <button class="btn btn-primary" id="btn-go-to-pay">缴费后台</button>
+            <button class="btn btn-primary" id="btn-go-to-info">报名数据</button>
+        </div>
+        <button class="btn btn-primary disabled" id="btn-export-excel">导出总表</button>
+        <button class="btn btn-warning" id="btn-logout">注销</button>
+        <button class="btn btn-danger disabled" id="btn-close-system">关闭系统</button>
+    <div class="col-sm-2"></div>
 </div>
 </body>
 </html>
@@ -28,5 +21,8 @@
     });
     $("#btn-logout").click(function() {
         window.location.assign("<?=site_url('admin/logout')?>");
+    });
+    $("#btn-go-to-info").click(function() {
+        window.location.assign("<?=site_url('admin/info')?>");
     });
 </script>
