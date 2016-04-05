8487137819f8c19b78cfd8e8cc3503da24e6dbc8
JeldorPKU <jack.jiang.capu@gmail.com>
2016-04-05 11:18:44 +0800

    Remove silly comments.

--- application/views/activate_error.php
+++ application/views/activate_error.php
@@ -1,7 +1,1 @@
-/**
- * Created by PhpStorm.
- * User: Jeldor
- * Date: 16/4/5
- * Time: 11:12
- */
 <h3>激活码不存在。</h3>
--- application/views/activate_footer.php
+++ application/views/activate_footer.php
@@ -1,8 +1,2 @@
-/**
- * Created by PhpStorm.
- * User: Jeldor
- * Date: 16/4/5
- * Time: 11:00
- */
 </body>
 </html>
