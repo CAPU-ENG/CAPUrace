7d84195b6631b186869bdb472ec0ab667aecd7a4
zhiyuecynthia <jinwanlin@pku.edu.cn>
2016-04-15 14:09:55 +0800

    video and award  update

--- application/views/race_info_racevideo.php
+++ application/views/race_info_racevideo.php
@@ -3,7 +3,9 @@
 <div class="content">
 	<h3 class="text-center">赛场视频</h3>
 	<hr>
-	<h4 class="text-center"><strong>待更新</strong></h4>
+	<div>
+		<iframe height="498" width="645" src="http://player.youku.com/embed/XOTI5ODkyMTU2" frameborder=0 allowfullscreen></iframe>
+	</div>
 
 </div>
 </div>
