d378725477e4a3ff99296a56f5fd641dc928cdc3
蒋雨蒙 <jack.jiang.capu@gmail.com>
2017-04-01 01:27:37 +0800

    Clean images directory (#259)
    * Remove useless images.
    
    * Remove useless comments.
    
    * Reorganize image file.
    
    * Modify .gitignore.

--- application/views/activate_header.php
+++ application/views/activate_header.php
@@ -3,6 +3,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>北大赛账户激活</title>
-<link rel="shortcut icon" href="/assets/images/capu.jpg">
+<link rel="shortcut icon" href="/assets/images/essentials/capu.jpg">
 </head>
 <body>
--- application/views/header_admin.php
+++ application/views/header_admin.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>管理员界面</title>
-<link rel="shortcut icon" href="/assets/images/capu.jpg">
+<link rel="shortcut icon" href="/assets/images/essentials/capu.jpg">
 <link href="<?=base_url()?>/assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?=base_url()?>/assets/css/style.css" rel="stylesheet">
 <script src="<?=base_url()?>/assets/js/jquery.min.js"></script>
