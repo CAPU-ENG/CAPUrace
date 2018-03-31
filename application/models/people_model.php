891a48b8f3a381b932df6608717b9576150c64e3
Mengcheng Jiang <mengchengj@pku.edu.cn>
2018-04-01 01:25:48 +0800

    add function get_race_quota

--- application/models/people_model.php
+++ application/models/people_model.php
@@ -180,6 +180,42 @@ class People_model extends CI_Model {
     }
 
     /*
+     * Get which race is above quota
+     */
+    public function get_race_quota() {
+        $query = $this->db->where('deleted', false)->where('ifrace', true)->get('people');
+        $team_num = 0;
+        $rdb_m_num = 0;
+        $rdb_f_num = 0;
+        $race_m_num = 0;
+        $race_f_num = 0;
+
+        $quota_results = array(
+            'team_status' => true,
+            'rdb_m_status' => true,
+            'rdb_f_status' => true,
+            'race_m_status' => true,
+            'race_f_status' => true
+         );
+        foreach ($query->result() as $row)
+        {
+            $team_num   +=  $row->ifteam;
+            $rdb_m_num  += $row->rdb;
+            $rdb_f_num  += $row->rdb_f;
+            $race_m_num += $row->race;
+            $race_f_num += $row->race_f;
+            
+        }
+        $quota_results['team_status'] = $team_num >= $GLOBALS['RACE_TEAM_QUOTA'] ? false : true;
+        $quota_results['rdb_m_status'] = $rdb_m_num >= $GLOBALS['RDB_M_QUOTA'] ? false : true;
+        $quota_results['rdb_f_status'] = $rdb_f_num >= $GLOBALS['RDB_F_QUOTA'] ? false : true;
+        $quota_results['race_m_status'] = $race_m_num >= $GLOBALS['RACE_M_QUOTA'] ? false : true;
+        $quota_results['race_f_status'] = $race_f_num >= $GLOBALS['RACE_F_QUOTA'] ? false : true;
+        return $quota_results;
+    }
+
+
+    /*
      * Clear unpaid RDB athletes.
      */
     public function clear() {
