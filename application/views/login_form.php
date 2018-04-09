f6df89deaccca782f97d758a9a4509426ef4ce06
fuguirong <fgr079@126.com>
2018-04-09 15:19:15 +0800

    Add the "forget password?" link in the sign-in form.

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -18,7 +18,7 @@
     </div>
     <br/>
     <br/>
-    <div class="col-sm-4" ><a href="<?=site_url('user/forgetPW')?>">忘记密码</a></div>
+    <div class="col-sm-4" ><a href="<?=site_url('user/forgetpw')?>">忘记密码</a></div>
     <br/>
     <br/>
     <hr/>
