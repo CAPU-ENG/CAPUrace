b3e27063ec0e9dfebfac0dcbfd3c303b5cd312d3
xiaoyaoxiangyun <44153101+xiaoyaoxiangyun@users.noreply.github.com>
2019-03-09 15:43:47 +0800

    Added temporary invitation mail
    the invitation content (and background) needs to be updated after the race agenda is established

--- application/libraries/MY_Email.php
+++ application/libraries/MY_Email.php
@@ -78,4 +78,38 @@ class MY_Email extends CI_Email
         $message = $school . '，<br><br>贵校车协交来的' . $fee . '元参赛费用已经收到，感谢你们对北大赛的大力支持！如有任何问题，请直接与各地区负责联系。' . $id_message;
         $this->send_mail($mail, $subject, $message);
     }
+
+    /*
+     * Send invitation email. 
+     * $subject and $message needs to be updated.
+    */
+    public function send_invitation_mail($mail, $school, $association_name) {
+        $subject = '第十六届全国高校自行车交流赛邀请函';
+        $id = $this->ci->user->get_id($mail);
+        $message = '
+            
+    <div style="background-image: url(\'https://s2.ax1x.com/2019/03/09/ASQKuF.md.jpg\'); background-size:100% 100%; width: 900px; height: 600px; z-index: -1; display: inline-block;">
+     </div>
+    <br><br><br>
+
+    <div style="background-image: url(\'https://s2.ax1x.com/2019/03/09/ASQ4Ej.md.jpg\'); background-size:100% 100%; width: 900px; height: 600px; z-index: -1; display: inline-block;">
+        <div style="position: relative; width: 600px; height: 400px; left: 300px; top: 100px;">
+            <p style="font-size: 25px; font-weight: bold;">
+                致 ' . $school . ' ' . $association_name . '：
+            </p>
+            <p style="font-size: 25px;">
+                北京大学自行车协会将于5月4日、5日和6日分别在北京大学和河北廊坊市固安县永定河自行车公园举办“北大车协 · 第十六届全国高校自行车交流赛”。本届比赛由xxx举办，并得到了xxx的大力支持。<br>
+                贵协作为全国知名的高校自行车运动社团，协会会员自行车运动水平优秀，学生素质高尚优良。我们诚挚邀请贵协参加“北大车协 · 第十六届全国高校自行车交流赛”， 以车会友，交流学习。
+            </p>
+
+            <p style="text-align: center; position: relative; left: 350px; width: 250px; font-size: 25px; font-weight: bold;">
+                北京大学自行车协会<br>
+                2018年4月
+            </p>
+            <img src="https://s2.ax1x.com/2019/03/09/ASQI5n.png" style="width: 150px; height: 50px;  position: relative; left: 400px; top: 0px;">
+        </div>
+    </div>
+        ';
+        $this->send_mail($mail, $subject, $message);
+    }
 }
