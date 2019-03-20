d4790d9a94347cc4b08fb1827895666ef5855608
韩子琦 <hanziqi@hanziqideMacBook-Pro.local>
2019-03-20 14:19:47 +0800

    competition-info-edit

--- capurace.sql
+++ capurace.sql
@@ -126,9 +126,9 @@ INSERT INTO `info` (`id`, `title`, `text`, `isdraft`) VALUES
   (5, 'race-info-award', NULL, 1),
   (6, 'activity', NULL, 1),
   (7, 'register-readme', NULL, 1),
-  (8, 'competition-info', NULL, 1);
-  (9, 'competition-info-history', NULL, 1);
-  (10, 'competition-info-sodality', NULL, 1);
+  (8, 'competition-info', NULL, 1),
+  (9, 'competition-info-history', NULL, 1),
+  (10, 'competition-info-sodality', NULL, 1),
   (11, 'competition-info-event', NULL, 1);
   
 --
