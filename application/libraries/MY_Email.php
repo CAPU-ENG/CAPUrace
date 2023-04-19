0665ffcff0730c5cdbd0e9e59971c148591db043
FFFomalhaut <chenjinyang0511@gmail.com>
2023-04-20 00:36:52 +0800

    修改激活邮件：添加链接

--- application/libraries/MY_Email.php
+++ application/libraries/MY_Email.php
@@ -64,7 +64,7 @@ class MY_Email extends CI_Email
         $link = site_url('user/activate') . '/' . $token;
         $id = $this->ci->user->get_id($mail);
         $id_message='<br><br>贵高校本次比赛的ID是<b>' . $id . '</b>，请领队同学务必牢记，并在比赛签到时出示。<br><br>祝好！<br><br>北京大学自行车协会';
-        $message = '请点击以下链接激活帐户' . $link . $id_message;
+        $message = '请点击以下链接激活帐户 ' . '<a href="' . $link . '">' . $link . '</a>' . $id_message;
         $this->send_mail($mail, $subject, $message);
     }
 
